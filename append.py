75eff0e9fa4ca3a5037368f75a6beafae270177a
SHANMUGAM161790 <58973567+SHANMUGAM161790@users.noreply.github.com>
2019-12-22 11:41:23 +0530

    Update append.py

--- append.py
+++ append.py
@@ -1,9 +1,10 @@
 a=input("enter the first array:")
 b=input("enter the second array:")
 c=input("enter the third array:")
-for i in range(len(a)):
+for i in range(len(a)):			#loop for appending the elements of first to second array 
 	b.append(a[i])
 print("combined array of a and b:",b)
-for j in range(len(b)):
-	c.append(b[j])
-print("total combined array of a,b and c:",c)
+for j in range(len(b)):			#loop for appending the third array elements which are divided by 2 
+	if(c[j]%2==0):
+		b.append(c[j])
+print("total combined array of a,b and c:",b)
