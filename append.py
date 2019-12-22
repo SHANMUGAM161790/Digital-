2a8f4b8a4414c25fc590db1151bc55e62203a7c0
SHANMUGAM161790 <58973567+SHANMUGAM161790@users.noreply.github.com>
2019-12-22 11:56:36 +0530

    Update append.py

--- append.py
+++ append.py
@@ -7,4 +7,4 @@ print("combined array of a and b:",a)
 for j in range(len(c)):			#loop for appending the third array elements which are divided by 2 
 	if(c[j]%2==0):
 		a.append(c[j])
-print("total combined array of a,b and c:",b)
+print("total combined array of a,b and c:",a)
