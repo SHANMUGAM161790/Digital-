13b37e1c35e164133af4071e11d289b8f015c557
SHANMUGAM161790 <58973567+SHANMUGAM161790@users.noreply.github.com>
2019-12-22 11:31:38 +0530

    Update append.py

--- append.py
+++ append.py
@@ -1,5 +1,9 @@
 a=input("enter the first array:")
 b=input("enter the second array:")
+c=input("enter the third array:")
 for i in range(len(a)):
 	b.append(a[i])
-print(b)
+print("combined array of a and b:",b)
+for j in range(len(b)):
+	c.append(b[j])
+print("total combined array of a,b and c:",c)
